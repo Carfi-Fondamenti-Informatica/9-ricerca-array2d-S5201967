ac4b7a344fa412c859f77c91e967deeb097b8046
S5201967 <91668770+S5201967@users.noreply.github.com>
2021-12-11 22:24:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include "lib.h"
 using namespace std;
-int cerca(char a[10][20], char nome[]);
+
 
 int main() {
-    char lista [10][20]={0};
-    char nome [20]={0};
-    cout<<"inserisci i 10 nomi (massimo 20 caratteri)"<<endl;
-    for(size_t i=0;i<10;i++) cin>>lista[i];
-    cout<<"inserisci nome (massimo 20 caratteri) da cercare:"<<endl;
-    nome[20];
-    for(int j=0;j<20;j++)  nome[j];
-    cin>>nome;
-
-    int posizione= cerca (lista,nome);
-    if(posizione >=0){
-        cout<<"presente in posizione;"<<posizione<<endl;
-       }else{
-        cout<<"non presente"<<endl;
+    int pos;
+    char a[10][20], f[1][20];
+    for(int i=0; i<10 ; i++){
+        cin >> a[i];
     }
+    cin >> f[0];
+
+    if (ricerca(a, f, pos)){
+        cout << pos << endl;
+    }else{
+        cout << "non presente" << endl;
+    }
+
+    return 0;
 }
