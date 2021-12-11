674d366d2800837b6c125bf83fa77802ab46d6bb
S5201967 <91668770+S5201967@users.noreply.github.com>
2021-12-11 21:03:25 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -4,7 +4,7 @@ int cerca (char a1[10][20], char a2[20]){
     for(int i=0;i<0;i++){
         bool flag=true;
         for(int j=0;j<20;j++){
-            if(a1[i][j]) != a2[j]) flag=false;
+            if(a1[i][j] != a2[j]) flag=false;
         }
         if (flag) return i;
     }
